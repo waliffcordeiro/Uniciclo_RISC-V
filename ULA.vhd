fb53e4b0eae8b213e0bc7bd5c868bce2a8b0d440
waliffcordeiro <43971264+waliffcordeiro@users.noreply.github.com>
2019-11-19 06:45:23 -0300

    ULA

---
+++ ULA.vhd
@@ -0,0 +1,43 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+entity ULA is
+	generic (DATA_WIDTH : natural := 32);
+	port (
+		A, B : in std_logic_vector(DATA_WIDTH -1 downto 0);
+		op : in std_logic_vector(3 downto 0);
+		result : out std_logic_vector(DATA_WIDTH -1 downto 0)
+	);
+end entity ULA;
+
+architecture ULA of ULA is
+
+-- Vetor com DATA_WITDH - 1 zeros, com objetivo de deixar o result genérico
+signal data_width_zero : std_logic_vector(DATA_WIDTH-2 downto 0);
+
+begin
+
+  -- Preenche com zeros
+  data_width_zero <= (others => '0');
+	
+	-- Calcula os resultados dependendo do opcode
+	result 	<=		std_logic_vector(signed(A) + signed(B)) when op = "0000" else --ADD
+				std_logic_vector(signed(A) - signed(B)) when op = "0001" else -- SUB
+				std_logic_vector(unsigned(A) sll to_integer(unsigned(B))) when op = "0010" else -- SLL 
+				data_width_zero & '1' when op = "0011" and (signed(A) < signed(B)) else  -- SLT
+				data_width_zero & '1' when op = "0100" and (unsigned(A) < unsigned(B)) else  -- SLTU
+				A xor B when op = "0101" else -- XOR
+				std_logic_vector(unsigned(A) srl to_integer(unsigned(B))) when op = "0110" else -- SRL
+				std_logic_vector(unsigned(A) sra to_integer(unsigned(B))) when op = "0111" else -- SRA
+				A or B when op = "1000" else -- OR
+				A and B when op = "1001" else -- AND
+				data_width_zero & '1' when op = "1010" and (unsigned(A) = unsigned(B)) else -- SEQ
+				data_width_zero & '1' when op = "1011" and (unsigned(A) /= unsigned(B)) else -- SNE
+				data_width_zero & '1' when op = "1100" and (signed(A) >= signed(B)) else --SGE
+				data_width_zero & '1' when op = "1101" and (unsigned(A) >= unsigned(B)) else -- SGEU
+
+				data_width_zero & '0'; -- Caso default dos opcodes, e caso não satisfeito à condição de set
+
+
+
+end ULA;
---
+++ teste_ULA.vhd
@@ -0,0 +1,210 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity ULA_test is
+	generic (DATA_WIDTH : natural := 32);
+end entity ULA_test;
+
+architecture arch_ULA_test of ULA_test is
+	component ULA is
+		port (
+			A, B : in std_logic_vector(DATA_WIDTH -1 downto 0);
+			op : in std_logic_vector(3 downto 0);
+			result : out std_logic_vector(DATA_WIDTH -1 downto 0)
+		);
+	end component;
+
+	signal A, B, result : std_logic_vector(DATA_WIDTH - 1 downto 0);
+	signal op : std_logic_vector(3 downto 0);
+	
+begin
+
+	test: ULA port map (A, B, op, result);
+	
+	process begin
+		-- Add
+		A <= x"00000005";
+		B <= x"00000002";
+		op <= "0000";
+		wait for 10 ns;
+		if (unsigned(result)=7) then report "Sucesso - Soma" severity note;
+		else report "Erro - Soma" severity error;
+		end if;
+
+		-- Sub
+		A <= x"00000005";
+		B <= x"00000002";
+		op <= "0001";
+		wait for 10 ns;
+		if (unsigned(result)=3) then report "Sucesso - Subtracao" severity note;
+		else report "Erro - Subtracao" severity error;
+		end if;
+
+		-- SLL
+		A <= x"00000004";
+		B <= x"00000001";
+		op <= "0010";
+		wait for 10 ns;
+		if (unsigned(result)=8) then report "Sucesso - SLL" severity note;
+		else report "Erro - SLL" severity error;
+		end if;
+
+		-- SLT A > B
+		A <= x"00000000";
+		B <= x"FFFFFFFF";
+		op <= "0011";
+		wait for 10 ns;
+		if (unsigned(result)=0) then report "Sucesso - SLT A > B" severity note;
+		else report "Erro - SLT A > B" severity error;
+		end if;
+
+		-- SLT A < B
+		A <= x"FFFFFFFF";
+		B <= x"00000000";
+		op <= "0011";
+		wait for 10 ns;
+		if (unsigned(result)=1) then report "Sucesso - SLT A < B" severity note;
+		else report "Erro - SLT A < B" severity error;
+		end if;
+
+		-- SLTU A > B
+		A <= x"FFFFFFFF";
+		B <= x"00000000";
+		op <= "0100";
+		wait for 10 ns;
+		if (unsigned(result)=0) then report "Sucesso - SLTU A > B" severity note;
+		else report "Erro - SLT A > B" severity error;
+		end if;
+
+		-- SLTU A < B
+		A <= x"00000000";
+		B <= x"FFFFFFFF";
+		op <= "0100";
+		wait for 10 ns;
+		if (unsigned(result)=1) then report "Sucesso - SLTU A < B" severity note;
+		else report "Erro - SLT A < B" severity error;
+		end if;
+
+		-- XOR
+		A <= x"00000000";
+		B <= x"F0F0F0F0";
+		op <= "0101";
+		wait for 10 ns;
+		if (result=x"F0F0F0F0") then report "Sucesso - Xor" severity note;
+		else report "Erro - Xor" severity error;
+		end if;
+		
+		-- SRL
+		A <= x"00000004";
+		B <= x"00000001";
+		op <= "0110";
+		wait for 10 ns;
+		if (unsigned(result)=2) then report "Sucesso - SRL" severity note;
+		else report "Erro - SRL" severity error;
+		end if;
+
+		-- SRA
+		A <= x"00000008";
+		B <= x"00000002";
+		op <= "0111";
+		wait for 10 ns;
+		if (unsigned(result)=2) then report "Sucesso - SRA" severity note;
+		else report "Erro - SRA" severity error;
+		end if;
+
+		-- OR
+		A <= x"00000000";
+		B <= x"F0F0F0F0";
+		op <= "1000";
+		wait for 10 ns;
+		if (result=x"F0F0F0F0") then report "Sucesso - OR" severity note;
+		else report "Erro - OR" severity error;
+		end if;
+
+		-- AND
+		A <= x"00000000";
+		B <= x"F0F0F0F0";
+		op <= "1001";
+		wait for 10 ns;
+		if (result=x"00000000") then report "Sucesso - AND" severity note;
+		else report "Erro - AND" severity error;
+		end if;
+
+		-- SEQ A == B
+		A <= x"000F0000";
+		B <= x"000F0000";
+		op <= "1010";
+		wait for 10 ns;
+		if (unsigned(result)=1) then report "Sucesso - SEQ A == B" severity note;
+		else report "Erro - SEQ A == B" severity error;
+		end if;
+
+		-- SEQ A != B
+		A <= x"000F000F";
+		B <= x"000F0000";
+		op <= "1010";
+		wait for 10 ns;
+		if (unsigned(result)=0) then report "Sucesso - SEQ A != B" severity note;
+		else report "Erro - SEQ A != B" severity error;
+		end if;
+
+
+		-- SNE A != B
+		A <= x"000F000F";
+		B <= x"000F0000";
+		op <= "1011";
+		wait for 10 ns;
+		if (unsigned(result)=1) then report "Sucesso - SNE A != B" severity note;
+		else report "Erro - SNE A != B" severity error;
+		end if;
+
+
+		-- SNE A == B
+		A <= x"000F000F";
+		B <= x"000F000F";
+		op <= "1011";
+		wait for 10 ns;
+		if (unsigned(result)=0) then report "Sucesso - SNE A == B" severity note;
+		else report "Erro - SNE A == B" severity error;
+		end if;
+
+		-- SGE  A >= B (Signed)
+		A <= x"00000000";
+		B <= x"FFFFFFFF";
+		op <= "1100";
+		wait for 10 ns;
+		if (unsigned(result)=1) then report "Sucesso - SGE A >= B" severity note;
+		else report "Erro - SGE A >= B" severity error;
+		end if;
+
+		-- SGE  A < B (Signed)
+		A <= x"FFFFFFFF";
+		B <= x"00000000";
+		op <= "1100";
+		wait for 10 ns;
+		if (unsigned(result)=0) then report "Sucesso - SGE A < B" severity note;
+		else report "Erro - SGE A < B" severity error;
+		end if;
+
+		-- SGEU  A >= B (unigned)
+		A <= x"FFFFFFFF";
+		B <= x"00000000";
+		op <= "1101";
+		wait for 10 ns;
+		if (unsigned(result)=1) then report "Sucesso - SGEU A >= B" severity note;
+		else report "Erro - SGEU A >= B" severity error;
+		end if;
+
+		-- SGEU  A < B (unsigned)
+		A <= x"00000000";
+		B <= x"FFFFFFFF";
+		op <= "1101";
+		wait for 10 ns;
+		if (unsigned(result)=0) then report "Sucesso - SGEU A < B" severity note;
+		else report "Erro - SGEU A < B" severity error;
+		end if;
+
+		wait; -- wait forever
+	end process;
+end arch_ULA_test;
