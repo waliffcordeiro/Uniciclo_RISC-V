b294a7fc8c5612819e527c819e0d31d7d113cd64
waliffcordeiro <43971264+waliffcordeiro@users.noreply.github.com>
2019-12-04 13:34:57 -0300

    Padronizando nome dos mux

--- mux2to1_32bits.vhd
+++ mux2to1_32bits.vhd
@@ -1,19 +1,19 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity mux2to1_32bits is
+entity mux2 is
 	generic(DATA_WIDTH : natural := 32);
 	port(
 		A, B : in std_logic_vector(DATA_WIDTH - 1 downto 0);
 		OP: in std_logic;
 		F: out std_logic_vector(DATA_WIDTH - 1 downto 0)
 	);
-end entity mux2to1_32bits;
+end entity mux2;
 
-architecture mux2to1_32bits of mux2to1_32bits is
+architecture arch_mux2 of mux2 is
 begin
 	with op select
 	f <= std_logic_vector(a) when '0', 
 		  std_logic_vector(b) when others;
 		 
-end mux2to1_32bits;
+end arch_mux2;
